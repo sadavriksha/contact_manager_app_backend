f5a01cd108be53412703be4b4cb16ddc0e73cdf3
Your Name <ssadagupta@mail.com>
2023-03-04 02:25:30 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+// mongodb://127.0.0.1:27017/Contact
 const connectDB = async () => {
   return mongoose
-    .connect('mongodb://127.0.0.1:27017/Contact')
+    .connect('mongodb://mongo:WD0aNs9uMovNxAiwkzE4@containers-us-west-150.railway.app:7992')
     .then(() => console.log(`connection to database established...`))
     .catch((err) => console.log(err));
 
